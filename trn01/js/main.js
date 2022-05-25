7fe84a0bf4b5c71427ef80ceacb0be79ee3ffe4b
taeseokyoung <105770887+taeseokyoung@users.noreply.github.com>
2022-05-25 18:39:40 +0900

    Add files via upload

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,12 +1,23 @@
 $(function () {
 
+    $('.main_slider').on('init reInit afterChange', function () {
+        var here = $('.slick-current');
+        here.addClass('on').siblings().removeClass('on')
+    })
+
     $('.main_slider').slick({
         arrows: false,
         autoplay: true,
         pauseOnHover: false,
         pauseOnFocus: false,
+        fade: true,
 
     })
+
+
 })
+
+
+
 
 
