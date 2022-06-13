0f7e55009b2bb8ff694884c2d3101e52726c7613
taeseokyoung <105770887+taeseokyoung@users.noreply.github.com>
2022-06-13 16:41:02 +0900

    Add files via upload

---
+++ html_run/js/slick01_prac.js
@@ -0,0 +1,19 @@
+$(function () {
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+    });
+
+    $('.slider02').slick({
+        slidesToShow: 3,
+    });
+
+    $('.visual_slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+    });
+
+})
